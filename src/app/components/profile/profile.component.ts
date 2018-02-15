27d1671b7fe5e7cd3969b10dee6951de27375155
Bryan Mesias <sevensand29@gmail.com>
2018-02-15 17:40:03 +0300

    deleteing profile component

--- src/app/components/profile/profile.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'profile',
-  templateUrl: './profile.component.html'
-})
-export class ProfileComponent {}
